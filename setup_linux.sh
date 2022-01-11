58dad90a1d9de271a00ea714a630c5e41f071d5e
Nick Butts <buttsnicholasl@johndeere.com>
2022-01-11 14:50:23 -0600

    Initial checkin.

---
+++ setup_linux.sh
@@ -0,0 +1,4 @@
+
+#!/bin/bash
+sudo apt install -y git build-essential openssh-server ffmpeg locate gstreamer1.0-plugins-bad gstreamer1.0-libav net-tools wireshark-qt pipenv nvidia-headless-470 nvidia-cuda-toolkit nvidia-utils-470 xterm autoconf libtool texinfo zlib1g:i386 libncurses5-dev
+
