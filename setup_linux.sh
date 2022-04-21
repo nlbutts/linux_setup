1d481bf50e99042dc601bfb85f20e18373fb5c12
Nick Butts <nlbutts@ieee.org>
2022-04-21 13:23:00 -0700

    Some updates to packages and groups.

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,4 +1,15 @@
 
 #!/bin/bash
-sudo apt install -y git build-essential openssh-server ffmpeg locate gstreamer1.0-plugins-bad gstreamer1.0-libav net-tools wireshark-qt pipenv nvidia-headless-470 nvidia-cuda-toolkit nvidia-utils-470 xterm autoconf libtool texinfo zlib1g:i386 libncurses5-dev
+sudo apt install -y git build-essential openssh-server ffmpeg locate gstreamer1.0-plugins-bad gstreamer1.0-libav net-tools wireshark-qt pipenv xterm autoconf libtool texinfo zlib1g:i386 libncurses5-dev docker.io
 
+if [[ $1 == "cuda" ]]; then
+  echo "Install NVidia drivers and cuda"
+  sudo apt install -y nvidia-headless-470 nvidia-cuda-toolkit nvidia-utils-470
+fi
+
+sudo usermod -a -G dialout $USER
+sudo usermod -a -G docker $USER
+sudo usermod -a -G wireshark $USER
+
+git config --global user.email "nlbutts@ieee.org"
+git config --global user.name "Nick Butts"
